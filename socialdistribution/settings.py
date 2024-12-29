f14a23f919025240b41e50d6ef9f2fd122e4322b
joyce omamuli <joycemacbookair2@joyces-MacBook-Air2.local>
2024-12-28 21:26:38 -0700

    static file problems

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -185,9 +185,9 @@ STATIC_FILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "staticfiles"),
+# )
 
 
 # Default primary key field type
